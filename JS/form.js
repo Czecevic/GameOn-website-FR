ede35f1b2cb628fc58c457f676b54798da4d60f7
Charles <charleszecevic@gmail.com>
2022-09-09 17:12:26 +0200

    modification css / html / js & a faire(replacer le bouton close)

--- JS/form.js
+++ JS/form.js
@@ -30,7 +30,98 @@ input.forEach((input) => {
                 lastChecker(e.target.value)
                 return
             case "email":
-                
+                emailChecker(e.target.value)
+                return
+            case "birthdate":
+                birthdateChecker(e.target.value)
+                return
+            case "quantity":
+                quantityChecker(e.target.value)
+                return
+            case "checkbox1":
+                checkboxChecker(e.target.value)
+                return
+            default:
+                null;
         }
-    })
-})
+    });
+});
+
+const firstChecker = (value) => {
+    const firstContainer = document.querySelector(".first-container");
+    const errorDisplay = document.querySelector(".first-container > span");
+    let isValid = false;
+
+    if (value.length < 2) {
+        firstContainer.classList.add("error")
+        errorDisplay.textContent = "Veuillez saisir plus de 2 charactères dans le champ prénom"
+    } else {
+        errorDisplay.textContent = ""
+        isValid = true
+    }
+    return isValid
+}
+
+const lastChecker = (value) => {
+    const lastContainer = document.querySelector(".last-container");
+    const errorDisplay = document.querySelector(".last-container > span");
+    let isValid = false;
+
+    if (value.length < 2) {
+        lastContainer.classList.add("error")
+        errorDisplay.textContent = "Veuillez saisir plus de 2 charactères dans le champ nom"
+    } else {
+        errorDisplay.textContent = ""
+        isValid = true
+    }
+    return isValid
+} 
+
+const emailChecker = (value) => {
+    const emailContainer = document.querySelector(".email-container");
+    const errorDisplay = document.querySelector(".email-container > span");
+    let isValid = false;
+
+    if (!value.match(/^[\w_-]+@[\w-]+\.[a-z]{2,4}$/i)) {
+        emailContainer.classList.add("error")
+        errorDisplay.textContent = "Veuillez verifier votre email"
+    } else {
+        errorDisplay.textContent = ""
+        isValid = true
+    }
+    return isValid
+}
+
+const birthdateChecker = (value) => {
+    const birthdateContainer = document.querySelector(".birthdate-container");
+    const errorDisplay = document.querySelector(".birthdate-container > span");
+    let isValid = false;
+
+    if (!value) {
+        birthdateContainer.classList.add("error")
+        errorDisplay.textContent = "Veuillez entrer votre date de naissance"
+    } else {
+        errorDisplay.textContent = ""
+        isValid = true
+    }
+    return isValid
+} 
+
+const quantityChecker = (value) => {
+    const birthdateContainer = document.querySelector(".quantity-container");
+    const errorDisplay = document.querySelector(".quantity-container > span");
+    let isValid = false;
+
+    if (!value) {
+        birthdateContainer.classList.add("error")
+        errorDisplay.textContent = "Veuillez entrer un chiffre"
+    } else {
+        errorDisplay.textContent = ""
+        isValid = true
+    }
+    return isValid
+} 
+
+const checkboxContainer = () => {
+    const errorDisplay = document.querySelector(".formData > small")
+}
--- JS/modalConfirm.js
+++ JS/modalConfirm.js
@@ -1,22 +1,26 @@
-const button_Submit = document.querySelector(".button-submit");
-const Form_Confirmation = document.querySelector(".Form_Confirmation");
-const Valid_Form = document.querySelector(".Valid_Form")
-const button_Submit_Confirm = document.querySelector(".button_Submit_Confirm")
 
-// Fonction permettant d'ouvrir le formulaire de confirmation d'inscription
 
+const btnSubmit = document.querySelector(".btn-submit");
+const modalConfirmation = document.querySelector(".formConfirmation");
+const spanValidModal = document.querySelector(".formConfirmation > span");
+const btnSubmitConfirm = document.querySelector(".btn-submit-confirm");
+
+/**
+ * Fonction pour ouvrir la modale de confirmation d'inscription
+ */
 function launchModalConfirmation() {
-    Form_Confirmation.style.display = "block";
-    Valid_Form.innerHTML = "Merci pour \n votre inscription";
+  modalConfirmation.style.display = "block";
+  spanValidModal.innerHTML = "Merci pour <br> votre inscription";
 }
 
-button_Submit.addEventListener("click", launchModalConfirmation)
+btnSubmit.addEventListener("click", launchModalConfirmation);
 
-// Fonction permettant de fermer le formulaire de confirmation d'inscription
-
+/**
+ * Fonction pour fermer la modale de confirmation d'inscription
+ */
 function closeModalConfirmation() {
-    Form_Confirmation.style.display = "none";
-    window.location.reload();
+  modalbg.style.display = "none";
+  window.location.reload();
 }
 
-button_Submit_Confirm.addEventListener("click", closeModalConfirmation)
+btnSubmitConfirm.addEventListener("click", closeModalConfirmation);
